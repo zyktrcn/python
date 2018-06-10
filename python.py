0bac2879b7b4ba8aad014ef0d2273344b3a66a09
zyktrcn <zyktrcn@gmail.com>
2018-06-10 17:11:22 +0800

    init

---
+++ python.py
@@ -0,0 +1,35 @@
+# char = 'S'
+# string = 'hello' + \
+#         'word'
+# print(char)
+# # print('#')
+# print(string)
+
+# input('enter: \n')
+
+# a, b = 1, 2
+# print(a+b, a-b, a/b, a//b, a%b, a**b)
+
+# s = 'hello word'
+# print(s[1:5], s*3, s[1])
+
+# list = [1, 'hello', 100.002, 'word']
+# list[1] = 'word'
+# list[3] = ['hello', 'word']
+# print(list, list[1], type(list), len(list))
+
+# tupe = (1001, 'hello', 'word', [100, 'string'], (2, 3))
+# print(tupe, tupe[1], type(tupe), len(tupe))
+
+# list = [1, 2, 3, 4]
+# def add(item):
+#     return item + 1
+# subList = [add(item) for item in list]
+# print(subList)
+
+# listO = [1, 2, 3, 4]
+# listT = [2, 3, 4, 5]
+# def add(o, t):
+#     return o*t
+# subList = [add(o, t) for t in listT for o in listO]
+# print(subList)
